02ce122ace2fc9b30afd54cf763cc97eba682987
Imanuel Habekotté <Yambam@users.noreply.github.com>
2016-08-29 22:52:36 +0200

    Translatable 404 error "Not found" message

--- error-404.php
+++ error-404.php
@@ -1,5 +1,5 @@
-<?php $no_modified_header = true; $page_title = "404 - Not found"; include("default-top.php"); ?>
-				<h2>404 - Not found</h2>
+<?php $no_modified_header = true; $page_title = "404 - " . gettext('Not found'); include("default-top.php"); ?>
+				<h2>404 - <?php echo gettext('Not found'); ?></h2>
 				<p>
-					The page you were looking for doesn't exist.
+					<?php echo gettext("The page you were looking for doesn't exist."); ?>
 <?php include("default-bottom.php"); ?>
