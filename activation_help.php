36797c02164854700a3f84615018f0edc95c5fc6
Imanuel Habekotté <Yambam@users.noreply.github.com>
2016-09-05 12:07:34 +0200

    Add "Reply-to" header to sent mail

--- activation_help.php
+++ activation_help.php
@@ -6,7 +6,8 @@
 If you are experiencing trouble activating your account, please contact the administrator using the following email address: ima.habekotte@gmail.com
 
 NOTE: This is automated message, do not try to reply to info@gamemaker.mooo.com! We won't be able to receive your message that way.";
-	$mail_headers = "From: info@gamemaker.mooo.com\r\n";
+	$mail_headers  = "From: info@gamemaker.mooo.com\r\n";
+	$mail_headers .= "Reply-To: ima.habekotte@gmail.com\r\n";
 	
 	/*
 	if (mail($mail_recipient,$mail_subject,$mail_content,$mail_headers)) {
